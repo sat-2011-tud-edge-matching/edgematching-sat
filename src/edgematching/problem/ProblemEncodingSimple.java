79fdb3756cb52623de3cc29a0b3ad571de985bb0
andreas <ajd@arcor.de>
2011-11-20 21:48:21 +0100

    encoding for corner pieces

--- src/edgematching/problem/ProblemEncodingSimple.java
+++ src/edgematching/problem/ProblemEncodingSimple.java
@@ -9,11 +9,30 @@ import edgematching.cnf.*;
 public class ProblemEncodingSimple extends Problem
 	implements SATSolvable
 {
+	/*
+	 * amounts of colors
+	 */
 	protected int m_amount_of_border_colors;
 	protected int m_amount_of_center_colors;
 
+	/*
+	 * list of all pieces
+	 */
 	protected ArrayList<Piece> m_pieces;
-	protected int m_pieces_current_position;
+
+	/*
+	 * numbers of pieces sorted by kind of piece
+	 */
+	protected ArrayList<Integer> m_corner_piece_numbers;
+	protected ArrayList<Integer> m_border_piece_numbers;
+	protected ArrayList<Integer> m_center_piece_numbers;
+
+	/*
+	 * numbers of places sorted by kind of place
+	 */
+	protected ArrayList<Integer> m_corner_place_numbers;
+	protected ArrayList<Integer> m_border_place_numbers;
+	protected ArrayList<Integer> m_center_place_numbers;
 
 	/*
 	 * simple constructor cloning the original problem
@@ -25,8 +44,7 @@ public class ProblemEncodingSimple extends Problem
 		m_amount_of_border_colors = m_border_colors.size ();
 		m_amount_of_center_colors = m_center_colors.size ();
 
-		m_pieces = new ArrayList<Piece> (m_grid_width * m_grid_height);
-		m_pieces_current_position = 0;
+		initNumberArrays ();
 	}
 
 	/*
@@ -68,26 +86,31 @@ public class ProblemEncodingSimple extends Problem
 	 * ===============================================================================
 	 */
 
+	/*
+	 * create clauses concerning corner pieces
+	 */
 	protected void encodeCornerPieces (CNFFormula formula)
 	{
-		int indexFirstPiece = m_pieces_current_position;
+		for (Integer i_piece : m_corner_piece_numbers) {
+			Clause tempClause = new Clause ();
 
-		for (Piece i_corner_piece : m_corner_pieces) {
-			m_pieces.add (m_pieces_current_position, i_corner_piece);
-
-			int[] tempClause = {
-				convertXijToSATVariable (m_pieces_current_position, convertXYToSubsequentNumber (0, 0)),
-				convertXijToSATVariable (m_pieces_current_position, convertXYToSubsequentNumber (m_grid_width - 1, 0)),
-				convertXijToSATVariable (m_pieces_current_position, convertXYToSubsequentNumber (0, m_grid_height - 1)),
-				convertXijToSATVariable (m_pieces_current_position, convertXYToSubsequentNumber (m_grid_width - 1, m_grid_height - 1))
-				};
+			for (Integer i_place : m_corner_place_numbers) {
+				tempClause.add (convertXijToSATVariable (i_piece, i_place));
+			}
 
 			formula.addClause (tempClause);
 
-			m_pieces_current_position ++;
+			for (Integer i_place : m_border_place_numbers) {
+				int[] tempArray = {convertXijToSATVariable (i_piece, i_place)};
+				formula.addClause (tempArray);
+			}
+
+			for (Integer i_place : m_center_place_numbers) {
+				int[] tempArray = {convertXijToSATVariable (i_piece, i_place)};
+				formula.addClause (tempArray);
+			}
 		}
 
-		int indexLastPiece = m_pieces_current_position - 1;
 	}
 
 	/*
@@ -117,5 +140,62 @@ public class ProblemEncodingSimple extends Problem
 	protected int convertSATVariableToPlace (int variable)
 	{
 		return (variable - 1) / (m_grid_width * m_grid_height);
+	}
+
+	/*
+	 * initialize arrays containing piece and place numbers...
+	 */
+	protected void initNumberArrays ()
+	{
+		m_pieces = new ArrayList<Piece> (m_grid_width * m_grid_height);
+
+		m_corner_piece_numbers = new ArrayList<Integer> (m_corner_pieces_count);
+		m_border_piece_numbers = new ArrayList<Integer> (m_border_pieces_count);
+		m_center_piece_numbers = new ArrayList<Integer> (m_center_pieces_count);
+
+		m_corner_place_numbers = new ArrayList<Integer> (m_corner_pieces_count);
+		m_border_place_numbers = new ArrayList<Integer> (m_border_pieces_count);
+		m_center_place_numbers = new ArrayList<Integer> (m_center_pieces_count);
+
+		int current_position = 0;
+
+		for (Piece i_corner_piece : m_corner_pieces) {
+			m_pieces.add (current_position, i_corner_piece);
+			m_corner_piece_numbers.add (current_position);
+			current_position ++;
+		}
+
+		for (Piece i_border_piece : m_border_pieces) {
+			m_pieces.add (current_position, i_border_piece);
+			m_border_piece_numbers.add (current_position);
+			current_position ++;
+		}
+
+		for (Piece i_center_piece : m_center_pieces) {
+			m_pieces.add (current_position, i_center_piece);
+			m_center_piece_numbers.add (current_position);
+			current_position ++;
+		}
+
+		m_corner_place_numbers.add (convertXYToSubsequentNumber (0, 0));
+		m_corner_place_numbers.add (convertXYToSubsequentNumber (0, m_grid_height - 1));
+		m_corner_place_numbers.add (convertXYToSubsequentNumber (m_grid_width - 1, 0));
+		m_corner_place_numbers.add (convertXYToSubsequentNumber (m_grid_width - 1, m_grid_height - 1));
+
+		for (int i_x = 1; i_x < m_grid_width - 1; i_x ++) {
+			m_border_place_numbers.add (convertXYToSubsequentNumber (i_x, 0));
+			m_border_place_numbers.add (convertXYToSubsequentNumber (i_x, m_grid_height - 1));
+		}
+
+		for (int i_y = 1; i_y < m_grid_height - 1; i_y ++) {
+			m_border_place_numbers.add (convertXYToSubsequentNumber (0, i_y));
+			m_border_place_numbers.add (convertXYToSubsequentNumber (m_grid_height - 1, i_y));
+		}
+
+		for (int i_x = 1; i_x < m_grid_width - 1; i_x ++) {
+			for (int i_y = 1; i_y < m_grid_height - 1; i_y ++) {
+				m_center_place_numbers.add (convertXYToSubsequentNumber (i_x, i_y));
+			}
+		}
 	}
 }
